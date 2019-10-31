0f23ead5fbff05d697e944d8d5c49533e60f6b89
lamb <l2q@ornl.gov>
2019-10-31 14:48:49 -0400

    updates to bfs OpenMP

--- codes/bfs_openmp_version/bfs.c
+++ codes/bfs_openmp_version/bfs.c
@@ -2,28 +2,25 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
-#include <omp.h>
 #include <stdbool.h>
-
-//#define NUM_THREAD 4
-#define OPEN
 
 int no_of_nodes;
 int edge_list_size;
+
 FILE *fp;
 
 //Structure to hold a node information
 struct Node
 {
-	int starting;
-	int no_of_edges;
+  int starting;
+  int no_of_edges;
 };
 
 void BFSGraph(int argc, char** argv);
 
 void Usage(int argc, char**argv){
 
-fprintf(stderr,"Usage: %s <num_threads> <input_file>\n", argv[0]);
+  fprintf(stderr,"Usage: %s <input_file>\n", argv[0]);
 
 }
 ////////////////////////////////////////////////////////////////////////////////
@@ -31,147 +28,181 @@ fprintf(stderr,"Usage: %s <num_threads> <input_file>\n", argv[0]);
 ////////////////////////////////////////////////////////////////////////////////
 int main( int argc, char** argv) 
 {
-	no_of_nodes=0;
-	edge_list_size=0;
-	BFSGraph( argc, argv);
+  no_of_nodes=0;
+  edge_list_size=0;
+  BFSGraph( argc, argv);
 }
 
 
 
 ////////////////////////////////////////////////////////////////////////////////
-//Apply BFS on a Graph using CUDA
+//Apply BFS on a Graph using OpenMP 
 ////////////////////////////////////////////////////////////////////////////////
 void BFSGraph( int argc, char** argv) 
 {
-    char *input_f;
-	int	 num_omp_threads;
-	
-	if(argc!=3){
-	Usage(argc, argv);
-	exit(0);
-	}
-    
-	num_omp_threads = atoi(argv[1]);
-	input_f = argv[2];
-	
-	printf("Reading File\n");
-	//Read in Graph from a file
-	fp = fopen(input_f,"r");
-	if(!fp)
-	{
-		printf("Error Reading graph file\n");
-		return;
-	}
+  char *input_f;
 
-	int source = 0;
+  int* h_cost;
+  int* h_graph_edges;
+  int source;
+  struct Node* h_graph_nodes;
+  bool *h_graph_mask;
+  bool *h_updating_graph_mask;
+  bool *h_graph_visited;
 
-	fscanf(fp,"%d",&no_of_nodes);
-   
-	// allocate host memory
-	struct Node* h_graph_nodes = (struct Node*) malloc(sizeof(struct Node)*no_of_nodes);
-	bool *h_graph_mask = (bool*) malloc(sizeof(bool)*no_of_nodes);
-	bool *h_updating_graph_mask = (bool*) malloc(sizeof(bool)*no_of_nodes);
-	bool *h_graph_visited = (bool*) malloc(sizeof(bool)*no_of_nodes);
+  int start, edgeno;   
+  int i,k;
+  bool stop;
+  int id,cost;
+  FILE *fpo;
 
-	int start, edgeno;   
-	// initalize the memory
-	for( unsigned int i = 0; i < no_of_nodes; i++) 
-	{
-		fscanf(fp,"%d %d",&start,&edgeno);
-		h_graph_nodes[i].starting = start;
-		h_graph_nodes[i].no_of_edges = edgeno;
-		h_graph_mask[i]=false;
-		h_updating_graph_mask[i]=false;
-		h_graph_visited[i]=false;
-	}
+  if(argc!=2){
+    Usage(argc, argv);
+    exit(0);
+  }
 
-	//read the source node from the file
-	fscanf(fp,"%d",&source);
-	source=0;
+  input_f = argv[1];
 
-	//set the source node as true in the mask
-	h_graph_mask[source]=true;
-	h_graph_visited[source]=true;
+  printf("Reading File\n");
+  //Read in Graph from a file
+  fp = fopen(input_f,"r");
+  if(!fp)
+  {
+    printf("Error Reading graph file\n");
+    return;
+  }
 
-	fscanf(fp,"%d",&edge_list_size);
+  source = 0;
 
-	int id,cost;
-	int* h_graph_edges = (int*) malloc(sizeof(int)*edge_list_size);
-	for(int i=0; i < edge_list_size ; i++)
-	{
-		fscanf(fp,"%d",&id);
-		fscanf(fp,"%d",&cost);
-		h_graph_edges[i] = id;
-	}
+  fscanf(fp,"%d",&no_of_nodes);
 
-	if(fp)
-		fclose(fp);    
+  // allocate host memory
+  h_graph_nodes = (struct Node*)malloc(sizeof(struct Node)*no_of_nodes);
+  h_graph_mask = (bool*) malloc(sizeof(bool)*no_of_nodes);
+  h_updating_graph_mask = (bool*) malloc(sizeof(bool)*no_of_nodes);
+  h_graph_visited = (bool*) malloc(sizeof(bool)*no_of_nodes);
+
+  // initalize the memory
+  for( i = 0; i < no_of_nodes; i++) 
+  {
+    fscanf(fp,"%d %d",&start,&edgeno);
+    h_graph_nodes[i].starting = start;
+    h_graph_nodes[i].no_of_edges = edgeno;
+  }
+
+  //read the source node from the file
+  fscanf(fp,"%d",&source);
+  source=0;
+
+  fscanf(fp,"%d",&edge_list_size);
+
+  h_graph_edges = (int*) malloc(sizeof(int)*edge_list_size);
+  for(i=0; i < edge_list_size ; i++)
+  {
+    fscanf(fp,"%d",&id);
+    fscanf(fp,"%d",&cost);
+    h_graph_edges[i] = id;
+  }
+
+  h_cost = (int*) malloc( sizeof(int)*no_of_nodes);
+
+  if(fp)
+    fclose(fp); 
+
+  printf("h_updating_graph_mask %i\n",h_updating_graph_mask[0]);
+  printf("h_graph_visited %i\n",h_graph_visited[0]);
+
+#pragma omp target data map(alloc:h_updating_graph_mask[0:no_of_nodes]) \
+  map(alloc:h_graph_mask[0:no_of_nodes],h_graph_visited[0:no_of_nodes]) \
+  map(alloc:h_graph_nodes[0:no_of_nodes], h_graph_edges[0:edge_list_size]) \
+  map(from:h_cost[0:no_of_nodes]) map(to:source)
+  {
+
+    #pragma omp target data map(to:h_graph_nodes[0:no_of_nodes]) // nowait
+    #pragma omp target teams distribute parallel for private(i)
+    for( i = 0; i < no_of_nodes; i++)
+    {
+      h_updating_graph_mask[i]=false;
+      h_graph_mask[i]=false;
+      h_graph_visited[i]=false;
+    }
+
+    #pragma omp target 
+    {
+      //set the source node as true in the mask
+      h_graph_mask[source]=true;
+      h_graph_visited[source]=true;
+    }
+
+    // allocate mem for the result on host side
+    #pragma omp target teams distribute parallel for 
+    for(i=0;i<no_of_nodes;i++) {
+      h_cost[i]=-1;
+      if(i == source) h_cost[source]=0;
+    }
+
+    // finish transfer node and edge to target
+    #pragma omp target map(to:h_graph_edges[0:edge_list_size])
+    //#pragma omp target depend(...) 
+
+    printf("Start traversing the tree\n");
+
+    k=0;
+
+    do
+    {
+      //if no thread changes this value then the loop stops
+      stop=false;
+      #pragma omp target teams distribute parallel for
+      for(int tid = 0; tid < no_of_nodes; tid++ )
+      {
+        if (h_graph_mask[tid] == true){ 
+          h_graph_mask[tid]=false;
+          for(i=h_graph_nodes[tid].starting; i<(h_graph_nodes[tid].no_of_edges + h_graph_nodes[tid].starting); i++)
+          {
+            id = h_graph_edges[i];
+            if(!h_graph_visited[id])
+            {
+              h_cost[id]=h_cost[tid]+1;
+              h_updating_graph_mask[id]=true;
+            }
+          }
+        }
+      }
+
+      #pragma omp target data map(tofrom:stop)
+      #pragma omp target teams distribute parallel for reduction(||:stop)
+      for(int tid=0; tid< no_of_nodes ; tid++ )
+      {
+        if (h_updating_graph_mask[tid] == true){
+          h_graph_mask[tid]=true;
+          h_graph_visited[tid]=true;
+          stop=true;
+          h_updating_graph_mask[tid]=false;
+        }
+      }
+      k++;
+
+      // copy stop variable back to host
+    } while(stop);
+
+  } /* end acc data */
+
+  //Store the result into a file
+  fpo = fopen("result.txt","w");
+  for(i=0;i<no_of_nodes;i++)
+    fprintf(fpo,"%d) cost:%d\n",i,h_cost[i]);
+  fclose(fpo);
+  printf("Result stored in result.txt\n");
 
 
-	// allocate mem for the result on host side
-	int* h_cost = (int*) malloc( sizeof(int)*no_of_nodes);
-	for(int i=0;i<no_of_nodes;i++)
-		h_cost[i]=-1;
-	h_cost[source]=0;
-	
-	printf("Start traversing the tree\n");
-	
-	int k=0;
-    
-	bool stop;
-	do
-	{
-		//if no thread changes this value then the loop stops
-		stop=false;
-
-#ifdef OPEN
-		omp_set_num_threads(num_omp_threads);
-		#pragma omp parallel for 
-#endif 
-		for(int tid = 0; tid < no_of_nodes; tid++ )
-		{
-			if (h_graph_mask[tid] == true){ 
-			h_graph_mask[tid]=false;
-			for(int i=h_graph_nodes[tid].starting; i<(h_graph_nodes[tid].no_of_edges + h_graph_nodes[tid].starting); i++)
-				{
-				int id = h_graph_edges[i];
-				if(!h_graph_visited[id])
-					{
-					h_cost[id]=h_cost[tid]+1;
-					h_updating_graph_mask[id]=true;
-					}
-				}
-			}
-		}
-
-  		for(int tid=0; tid< no_of_nodes ; tid++ )
-		{
-			if (h_updating_graph_mask[tid] == true){
-			h_graph_mask[tid]=true;
-			h_graph_visited[tid]=true;
-			stop=true;
-			h_updating_graph_mask[tid]=false;
-			}
-		}
-		k++;
-	}
-	while(stop);
-
-	//Store the result into a file
-	FILE *fpo = fopen("result.txt","w");
-	for(int i=0;i<no_of_nodes;i++)
-		fprintf(fpo,"%d) cost:%d\n",i,h_cost[i]);
-	fclose(fpo);
-	printf("Result stored in result.txt\n");
-
-
-	// cleanup memory
-	free( h_graph_nodes);
-	free( h_graph_edges);
-	free( h_graph_mask);
-	free( h_updating_graph_mask);
-	free( h_graph_visited);
-	free( h_cost);
+  // cleanup memory
+  free( h_graph_nodes);
+  free( h_graph_edges);
+  free( h_graph_mask);
+  free( h_updating_graph_mask);
+  free( h_graph_visited);
+  free( h_cost);
 
 }
 
